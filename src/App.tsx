6ce3b3f3a3e20ce55b31d11e2c7317779d8b2ff1
[dyad] <sramos96@proton.me>
2026-02-23 22:02:59 -0300

    [dyad] Corrigindo importação do Toaster no App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "react-hot-toast";
+import { Toaster } from "sonner";
 import Layout from "./components/Layout";
 import Index from "./pages/Index";
 import Jobs from "./pages/Jobs";
@@ -28,7 +28,7 @@ function App() {
           </Route>
         </Routes>
       </Router>
-      <Toaster position="top-right" />
+      <Toaster position="top-right" richColors />
     </QueryClientProvider>
   );
 }
