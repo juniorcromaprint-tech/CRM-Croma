82a769577f196018f9a26a9b979ec3724f484b53
sramos-pix <sramos96@proton.me>
2026-03-14 18:29:02 -0300

    perf: lazy-load clientes routes

--- src/routes/clientesRoutes.tsx
+++ src/routes/clientesRoutes.tsx
@@ -1,10 +1,13 @@
-import { Route } from 'react-router-dom';
-import ClientesPage from '@/domains/clientes/pages/ClientesPage';
-import ClienteDetailPage from '@/domains/clientes/pages/ClienteDetailPage';
+import { lazy } from "react";
+import { Route } from "react-router-dom";
+import LazyPage from "@/shared/components/LazyPage";
+
+const ClientesPage = lazy(() => import("@/domains/clientes/pages/ClientesPage"));
+const ClienteDetailPage = lazy(() => import("@/domains/clientes/pages/ClienteDetailPage"));
 
 export const clientesRoutes = (
   <>
-    <Route path="clientes" element={<ClientesPage />} />
-    <Route path="clientes/:id" element={<ClienteDetailPage />} />
+    <Route path="clientes" element={<LazyPage><ClientesPage /></LazyPage>} />
+    <Route path="clientes/:id" element={<LazyPage><ClienteDetailPage /></LazyPage>} />
   </>
 );
