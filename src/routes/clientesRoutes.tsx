ff02c79cbb9e8662555757666f6cb8bc6a036f31
juniorcromaprint-tech <junior.cromaprint@gmail.com>
2026-03-18 20:45:02 -0300

    chore: remove 9 dead component files (never imported)

--- src/routes/clientesRoutes.tsx
+++ src/routes/clientesRoutes.tsx
@@ -1,13 +1,14 @@
 import { lazy } from "react";
 import { Route } from "react-router-dom";
 import LazyPage from "@/shared/components/LazyPage";
+import PermissionGuard from "@/shared/components/PermissionGuard";
 
 const ClientesPage = lazy(() => import("@/domains/clientes/pages/ClientesPage"));
 const ClienteDetailPage = lazy(() => import("@/domains/clientes/pages/ClienteDetailPage"));
 
 export const clientesRoutes = (
   <>
-    <Route path="clientes" element={<LazyPage><ClientesPage /></LazyPage>} />
-    <Route path="clientes/:id" element={<LazyPage><ClienteDetailPage /></LazyPage>} />
+    <Route path="clientes" element={<PermissionGuard module="clientes" action="ver"><LazyPage><ClientesPage /></LazyPage></PermissionGuard>} />
+    <Route path="clientes/:id" element={<PermissionGuard module="clientes" action="ver"><LazyPage><ClienteDetailPage /></LazyPage></PermissionGuard>} />
   </>
 );
